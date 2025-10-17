676f38e4578284e827e15832d7bc4bf5a99668fd
andresospinacg <100716577+andresospinacg@users.noreply.github.com>
2025-10-17 17:07:45 -0500

    Se crea proyecto, se crea Splash y se configura redireccionamiento.

---
+++ app/src/main/java/com/app_arte_entre_puntadas/activities/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.app_arte_entre_puntadas.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app_arte_entre_puntadas.MainActivity
+import com.app_arte_entre_puntadas.R
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DURATION = 2000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DURATION)
+    }
+}
